d2fb9e3fd9e5d2aa5abf7fe7a1ca5d941bfc8521
asafspi <asafspi@gmail.com>
2016-12-07 09:41:14 +0200

    first commit

--- mylibrary/src/main/java/com/devlabs695/mylibrary/MainActivity.java
+++ mylibrary/src/main/java/com/devlabs695/mylibrary/MainActivity.java
@@ -3,6 +3,7 @@ package com.devlabs695.mylibrary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
-        Toast.makeText(this, intent.getStringExtra("joke"), Toast.LENGTH_SHORT).show();
+        String joke = intent.getStringExtra("joke");
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+        TextView jokeEditText = (TextView)findViewById(R.id.jokeTextView);
+        jokeEditText.setText(joke);
     }
 }
